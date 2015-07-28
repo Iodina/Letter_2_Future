db7e0b7be840deb3f78a67b7452b90eb22f64468
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2015-07-28 14:26:35 +0300

    Changed settings/production

--- FutureLetter/settings/production.py
+++ FutureLetter/settings/production.py
@@ -19,10 +19,10 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
