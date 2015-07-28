2f83811d57a0b9ef85a46b53d03024619ab1b0ef
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2015-07-28 14:08:36 +0300

    Requirements changed

--- FutureLetter/settings/production.py
+++ FutureLetter/settings/production.py
@@ -1,15 +1,15 @@
 # Parse database configuration from $DATABASE_URL
 import os
 from django.conf import settings
-
+import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = True
 
 DATABASES=settings.DATABASES
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
